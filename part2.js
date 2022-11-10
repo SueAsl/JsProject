f357c890f9d648f7cb52be504daa565b62f6ecc7
Syuzi <syuzi.aslanyan1999@gmail.com>
2022-11-10 20:10:29 +0400

    2nd part of homework

---
+++ part2.js
@@ -0,0 +1,11 @@
+let number = 2;
+let degree = 1
+    while (degree<=12){
+        console.log(number**degree)
+        degree++
+    };  //creating with loop
+
+
+
+let a = 2**12;
+console.log(a); //creating with just variable
